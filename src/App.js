a93dbf2ea8490588cc72e14d74ce390923ec614e
Rapuru <anil.rapuru@capgemini.com>
2021-10-14 09:37:15 +0530

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [mail, setMail] = useState('');
+  const [mailHistory, setMailHistory] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        onChange={(e) => {
+          setMail(e.target.value);
+          setMailHistory([...mailHistory, mail]);
+        }}
+      />
+      <br />
+      {mail}
+      <h3>History</h3>
+      <br />
+      {mailHistory.map((mailItem) => { return <div>{mailItem}</div> })}
     </div>
-  );
+  )
 }
 
 export default App;
