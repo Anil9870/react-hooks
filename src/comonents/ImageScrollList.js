f978c27c74e5ae35a5672ba45a88c87f90673e55
Rapuru <anil.rapuru@capgemini.com>
2021-10-18 09:13:20 +0530

    useReducer and useContext

--- src/comonents/ImageScrollList.js
+++ src/comonents/ImageScrollList.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import UseEffectImageScroll from './UseEffectImageScroll';
 
+export const ConfigContext = React.createContext();
+
+var universe = {
+    "name":"Anil Rapuru"
+}
+
 const ImageScrollList = () => {
     return (
+        <ConfigContext.Provider value={universe}>
         <div>
             <UseEffectImageScroll primary="images/pic1.jpg" secondary="images/pic2.jpg"/>
             <UseEffectImageScroll primary="images/pic1.jpg" secondary="images/pic2.jpg"/>
@@ -15,6 +22,7 @@ const ImageScrollList = () => {
             <UseEffectImageScroll primary="images/pic1.jpg" secondary="images/pic2.jpg"/>
             <UseEffectImageScroll primary="images/pic1.jpg" secondary="images/pic2.jpg"/>
         </div>
+        </ConfigContext.Provider>
     )
 }
 
--- src/comonents/UseEffectImageScroll.js
+++ src/comonents/UseEffectImageScroll.js
@@ -1,17 +1,37 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext, useReducer } from 'react';
+import { ConfigContext } from './ImageScrollList';
 
 const UseEffectImageScroll = (props) => {
     const [isInView, setInView] = useState(false);
 
     const imageRef = useRef(null);
 
+    const value = useContext(ConfigContext);
+
+    const setArrayReducer = (state, action) => {
+        switch (action.type) {
+            case "setArrayList":
+                return action.data;
+            default:
+                return state;
+        }
+    }
+
+    const [arrayOfNos, dispatchCustom] = useReducer(setArrayReducer, [])
+
 
     useEffect(() => {
+        console.log('use Effect');
         setInView(getInView());
         window.addEventListener('scroll', scrollHandler);
+        dispatchCustom({
+            type: "setArrayList",
+            data: [1, 2, 3, 4, 5]
+        });
         return () => {
             window.removeEventListener('scroll', scrollHandler);
         };
+
     }, []);
 
     const getInView = () => {
@@ -28,6 +48,9 @@ const UseEffectImageScroll = (props) => {
             <img
                 ref={imageRef}
                 width="20%" height="20%" src={isInView ? props.primary : props.secondary} alt="anil" />
+            {console.log("render")}
+            <h1>{value.name}</h1>
+            {arrayOfNos.map((no) => <h3>{no}</h3>)}
         </div>
     )
 }
